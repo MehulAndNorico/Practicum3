3aaf1c8baedf0e09e9c3f138aa2b331caf8a8d50
Norico Groeneveld <norico.g@live.com>
2015-04-03 14:44:11 +0200

    Bijna klaar

--- application/views/head.php
+++ application/views/head.php
@@ -4,15 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="nl">
 	<head>
 		<meta charset="utf-8">
-		<title>DataDate</title>
+		<title>DataDate: vind de leven van je liefde</title>
 		<link rel="stylesheet" type="text/css" href="/~4301358/wt3/public/style.css"/>
 	</head>
 	<body>
 		<div id="header">
 			<div id="logo">
 				<a href="start"><img src="/~4301358/wt3/public/logo.png"/></a>
-				&nbsp;
-				<h1>Vind de leven van je liefde</h1>
 			</div>
 			<?php
 				if (true)
@@ -25,4 +23,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				}
 			?>
 		</div>
-		<div id="content">
+		<div id="spacer">
+			<img src="/~4301358/wt3/public/logo.png"/>
+		</div>
--- application/views/start.php
+++ application/views/start.php
@@ -1,69 +1,71 @@
-<div id="promo">
-	<div id="promotekst">
-		<h3>Welkom bij DataDate</h3>
-		<p>
-			"De bijzonderheid in onze site is dat ons dating paradigma gebaseerd is op een unieke en wetenschappelijk correct bewezen profilerings- en matching techniek die zowel de persoonlijkheid als de lifestyle in de "dating equation" meeneemt en leert van de voorkeuren van de gebruiker om de dating ervaring te optimaliseren. Tot op zekere hoogte is dit al eens eerder vertoond (BrandDating.nl voor dating op basis van lifestyle - deze site is kennelijk wegen succes beëindigd - en Parship voor dating op basis van persoonlijkheid), maar wij hebben niet alleen betere technologieën; we combineren ook nog eens deze systemen en breiden ze uit met 'playing field changing' zelflerende functionaliteit."
-			<a href="http://www.cs.uu.nl/docs/vakken/b2wt/opdracht3.html">, aldus onze opdrachtgever.</a>
-		</p>
+<div id="content">
+	<div id="promo">
+		<div id="promotekst">
+			<h3>Vind de leven van je liefde</h3>
+			<p>
+				"De bijzonderheid in onze site is dat ons dating paradigma gebaseerd is op een unieke en wetenschappelijk correct bewezen profilerings- en matching techniek die zowel de persoonlijkheid als de lifestyle in de "dating equation" meeneemt en leert van de voorkeuren van de gebruiker om de dating ervaring te optimaliseren. Tot op zekere hoogte is dit al eens eerder vertoond (BrandDating.nl voor dating op basis van lifestyle - deze site is kennelijk wegen succes beëindigd - en Parship voor dating op basis van persoonlijkheid), maar wij hebben niet alleen betere technologieën; we combineren ook nog eens deze systemen en breiden ze uit met 'playing field changing' zelflerende functionaliteit."
+				<a href="http://www.cs.uu.nl/docs/vakken/b2wt/opdracht3.html">, aldus onze opdrachtgever.</a>
+			</p>
+		</div>
 	</div>
+	<div id="profielen">
+		<a href="inschrijven"><h3>Schrijf je nu in en ontmoet onder andere deze mensen:</h3></a>
+		<div class="profiel samen">
+			<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
+			<div class="part">
+				<p>NICKNAME GESLACHT 99</p>
+				<p>Persoonlijkheidstype: Je moeder</p>
+				<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
+				<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
+			</div>
+		</div>
+		<div class="profiel samen">
+			<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
+			<div class="part">
+				<p>NICKNAME GESLACHT 99</p>
+				<p>Persoonlijkheidstype: Je moeder</p>
+				<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
+				<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
+			</div>
+		</div>
+		<br>
+		<div class="profiel samen">
+			<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
+			<div class="part">
+				<p>NICKNAME GESLACHT 99</p>
+				<p>Persoonlijkheidstype: Je moeder</p>
+				<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
+				<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
+			</div>
+		</div>
+		<div class="profiel samen">
+			<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
+			<div class="part">
+				<p>NICKNAME GESLACHT 99</p>
+				<p>Persoonlijkheidstype: Je moeder</p>
+				<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
+				<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
+			</div>
+		</div>
+		<br>
+		<div class="profiel">
+			<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
+			<div class="part">
+				<p>NICKNAME GESLACHT 99</p>
+				<p>Persoonlijkheidstype: Je moeder</p>
+				<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
+				<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
+			</div>
+		</div>
+		<br>
+		<div class="profiel">
+			<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
+			<div class="part">
+				<p>NICKNAME GESLACHT 99</p>
+				<p>Persoonlijkheidstype: Je moeder</p>
+				<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
+				<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
+			</div>
+		</div>
+	</div>	
 </div>
-<div id="profielen">
-	<a href="inschrijven"><h3>Schrijf je nu in en ontmoet onder andere deze mensen:</h3></a>
-	<div class="profiel samen">
-		<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
-		<div class="part">
-			<p>NICKNAME GESLACHT 99</p>
-			<p>Persoonlijkheidstype: Je moeder</p>
-			<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
-			<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
-		</div>
-	</div>
-	<div class="profiel samen">
-		<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
-		<div class="part">
-			<p>NICKNAME GESLACHT 99</p>
-			<p>Persoonlijkheidstype: Je moeder</p>
-			<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
-			<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
-		</div>
-	</div>
-	<br>
-	<div class="profiel samen">
-		<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
-		<div class="part">
-			<p>NICKNAME GESLACHT 99</p>
-			<p>Persoonlijkheidstype: Je moeder</p>
-			<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
-			<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
-		</div>
-	</div>
-	<div class="profiel samen">
-		<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
-		<div class="part">
-			<p>NICKNAME GESLACHT 99</p>
-			<p>Persoonlijkheidstype: Je moeder</p>
-			<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
-			<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
-		</div>
-	</div>
-	<br>
-	<div class="profiel">
-		<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
-		<div class="part">
-			<p>NICKNAME GESLACHT 99</p>
-			<p>Persoonlijkheidstype: Je moeder</p>
-			<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
-			<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
-		</div>
-	</div>
-	<br>
-	<div class="profiel">
-		<img class="part" src="/~4301358/wt3/public/profielfoto.jpg" alt="Profielfoto van ...">
-		<div class="part">
-			<p>NICKNAME GESLACHT 99</p>
-			<p>Persoonlijkheidstype: Je moeder</p>
-			<p>Beschrijving: Hallo, dit is de eerste zin van mij beschrijving en omdat het een voorbeeld is ook de laatste</p>
-			<p>Merkvoorkeuren: Hier komt dus een kort lijstje met wat merken omdat dat moet</p>
-		</div>
-	</div>
-</div>	
